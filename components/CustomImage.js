cef0cd0db5f086bc65676c940e6deca258ba52a4
biaksangmunsong <biaksangmunsong@gmail.com>
2021-01-11 22:18:01 +0530

    image optomization added

---
+++ components/CustomImage.js
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import PropTypes from "prop-types"
+
+const CustomImage = props => {
+
+    // If the image is local it will start with a "/"
+    // Otherwise it'll be something like "https://"
+
+    return props.src.startsWith("/") ?
+    <div className="image__wrapper optimized">
+        <Image
+            src={ props.src }
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+        />
+    </div> :
+    <div className="image__wrapper not-optimized">
+        <img { ...props }/>
+    </div>
+
+}
+
+CustomImage.propTypes = {
+    src: PropTypes.node.isRequired
+}
+
+export default CustomImage
