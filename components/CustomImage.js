7f7568e3afe9774a125b8cd2430f505fda7b2ff8
biaksangmunsong <biaksangmunsong@gmail.com>
2021-01-11 23:14:36 +0530

    experimenting

--- components/CustomImage.js
+++ components/CustomImage.js
@@ -6,7 +6,7 @@ const CustomImage = props => {
     // If the image is local it will start with a "/"
     // Otherwise it'll be something like "https://"
 
-    return props.src.startsWith("/") ?
+    return !props.src.startsWith("http") ?
     <div className="image__wrapper optimized">
         <Image
             src={ props.src }
