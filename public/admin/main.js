0d1c90a0ea2709bf3bda7bffae1aa170ca47acc9
biaksangmunsong <biaksangmunsong@gmail.com>
2021-01-12 18:34:41 +0530

    experimenting

---
+++ public/admin/main.js
@@ -0,0 +1,17 @@
+CMS.registerPreviewStyle("/admin/styles/page-preview.css")
+
+const PagePreview = createClass({
+    render: function(){
+        var entry = this.props.entry
+        var body = entry.getIn(["data", "body"])
+        console.log(body)
+    
+        return h(
+            "div",
+            { className: "about_page_content" },
+            this.props.widgetFor("body")
+        )
+    }
+})
+
+CMS.registerPreviewTemplate("about_page", PagePreview)
