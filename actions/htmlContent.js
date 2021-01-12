35ac4c6d73f0497dd30f082a68536dca1e1a73c4
biaksangmunsong <biaksangmunsong@gmail.com>
2021-01-12 06:04:04 +0530

    added cloudinary

--- actions/htmlContent.js
+++ actions/htmlContent.js
@@ -2,7 +2,6 @@ import unified from "unified"
 import parse from "remark-parse"
 import remark2react from "remark-react"
 import CustomLink from "../components/CustomLink"
-import CustomImage from "../components/CustomImage"
 
 const htmlContent = content => {
 
@@ -10,8 +9,7 @@ const htmlContent = content => {
     .use(parse)
     .use(remark2react, {
       remarkReactComponents: {
-        a: CustomLink,
-        img: CustomImage
+        a: CustomLink
       }
     })
     .processSync(content).result
